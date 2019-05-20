82b02b6b125cbf1e1fd6ca14452a23755812244d
Julian Jabs <julian.jabs@uni-tuebingen.de>
2019-05-20 10:57:13 +0200

    Add some line breaks

--- lecturenotes/07-fae.scala
+++ lecturenotes/07-fae.scala
@@ -1,19 +1,28 @@
 /**
 Higher-Order Functions
 ======================
-F1-WAE, the language with first-order functions, lets us abstract over patterns that involve numbers. But what if we want to abstract over patterns that involve functions, such as the "list fold" pattern, whose instantiations include summing or multiplying a list of integers?
+F1-WAE, the language with first-order functions, lets us abstract over patterns that involve numbers. But what if we want to abstract
+over patterns that involve functions, such as the "list fold" pattern, whose instantiations include summing or multiplying a list of
+integers?
 
-To enable this kind of abstraction, we need to make functions "first-class", which means that they become values that can be passed to or returned from functions or stored in data structures. Languages with first-class functions enable so-called "higher-order functions", which are functions that accept or return a (possibly again higher-order) function.
+To enable this kind of abstraction, we need to make functions "first-class", which means that they become values that can be passed to
+or returned from functions or stored in data structures. Languages with first-class functions enable so-called "higher-order functions",
+which are functions that accept or return a (possibly again higher-order) function.
 
-We will see that this extension will make our language both simpler and much more powerful. This seeming contradiction is famously addressed by the first sentence of the Scheme language specification:
+We will see that this extension will make our language both simpler and much more powerful. This seeming contradiction is famously
+addressed by the first sentence of the Scheme language specification:
 
-"Programming languages should be designed not by piling feature on top of feature, but by removing the weaknesses and restrictions that make additional features appear necessary."
+"Programming languages should be designed not by piling feature on top of feature, but by removing the weaknesses and restrictions that
+make additional features appear necessary."
 
-The simplicity is due to the fact that this language is so expressive that many other language features can be "encoded", i.e., they do not need to be added to the language but can be expressed with the existing features.
+The simplicity is due to the fact that this language is so expressive that many other language features can be "encoded", i.e., they do
+not need to be added to the language but can be expressed with the existing features.
 
-This language, which we call "FAE", is basically the so-called "lambda calculus", a minimal but powerful programming language that has been highly influential in the design and theory of programming languages.
+This language, which we call "FAE", is basically the so-called "lambda calculus", a minimal but powerful programming language that has
+been highly influential in the design and theory of programming languages.
 
-FAE is the language of arithmetic expressions, AE, plus only two additional language constructs: Function abstraction and function application.
+FAE is the language of arithmetic expressions, AE, plus only two additional language constructs: Function abstraction and function
+application.
 */
 
 
@@ -31,11 +40,14 @@ case class Fun(param: Symbol, body: Exp) extends Exp
 case class App (funExpr: Exp, argExpr: Exp) extends Exp
 
 /**
-Due to the lambda calculus, the concrete syntax for function abstraction is often written with a lambda, such as ``lambda x. x+3``, thus also called lambda abstraction. The Scala syntax for lambda terms is ``(x) => x+3``, the Haskell syntax is ``\x -> x+3``.
+Due to the lambda calculus, the concrete syntax for function abstraction is often written with a lambda, such as ``lambda x. x+3``,
+thus also called lambda abstraction. The Scala syntax for lambda terms is ``(x) => x+3``, the Haskell syntax is ``\x -> x+3``.
 
-The concrete syntax for function application is often either  juxtaposition ``f a`` or using brackets ``f(a)``. Haskell and the lambda calculus use the former, Scala uses the latter.
+The concrete syntax for function application is often either  juxtaposition ``f a`` or using brackets ``f(a)``. Haskell and the
+lambda calculus use the former, Scala uses the latter.
 
-The ``with`` construct is not needed anymore since it can be encoded using ``App`` and ``Fun``. For instance, ``with x = 7 in x+3`` can be encoded (using Scala syntax) as ``((x) => x+3)(7)``
+The ``with`` construct is not needed anymore since it can be encoded using ``App`` and ``Fun``. For instance, ``with x = 7 in x+3``
+can be encoded (using Scala syntax) as ``((x) => x+3)(7)``
 
 We make this idea explicit by giving a constructive translation. Such translations are also often called "desugaring".
 */
@@ -65,7 +77,8 @@ assert( subst(Add(5,'x), 'y, 7) == Add(5,'x))
 assert( subst(Fun('x, Add('x,'y)), 'x, 7) == Fun('x, Add('x,'y)))
 
 /**
-However, what happens if ``e2`` contains free variables? The danger here is that they may be accidentially "captured" by the substitution. For instance, consider
+However, what happens if ``e2`` contains free variables? The danger here is that they may be accidentially "captured" by the substitution.
+For instance, consider
 
     ``subst(Fun('x, Add('x,'y)), 'y, Add('x,5))``
 
@@ -73,10 +86,12 @@ The result is ``Fun('x,Add('x,Add('x,5)))``
 
 This is not desirable, since it violates again static scoping.
 
-Note that this problem did not show up in earlier languages, because there we only substituted variables by numbers, but not by expressions that may contain free variables: The type of ``e2`` was ``Num`` and not ``Exp``.
+Note that this problem did not show up in earlier languages, because there we only substituted variables by numbers, but not by
+expressions that may contain free variables: The type of ``e2`` was ``Num`` and not ``Exp``.
 
-Hence we are still not done with defining substitution. But what is the desired result of the substitution above? The answer is that we must avoid the name clash by renaming the variable
- bound by the "lambda" if the variable name occurs free in ``e2``. This new variable name should be "fresh", i.e., not occur free in ``e2``.
+Hence we are still not done with defining substitution. But what is the desired result of the substitution above? The answer is that
+we must avoid the name clash by renaming the variable bound by the "lambda" if the variable name occurs free in ``e2``.
+This new variable name should be "fresh", i.e., not occur free in ``e2``.
 
 For instance, in the example above, we could first rename ``'x`` to the fresh name ``'x0`` and only then substitute, i.e.
 
@@ -131,9 +146,12 @@ But how do we evaluate a function abstraction? Obviously we cannot return a numb
 
 We realize that functions are also values! Hence we have to broaden the return type of our evaluator to also allow functions as values.
 
-For simplicity, we use "Exp" as our return type since it allows us to return both numbers and functions. Later we will become more sophisticated.
+For simplicity, we use "Exp" as our return type since it allows us to return both numbers and functions. Later we will become more
+sophisticated.
 
-This means that a new class of errors can occur: A subexpression evaluates to a number where a function is expected, or vice versa. Such errors are called _type errors_, and we will talk about them in much more detail later. For now, it means that we need to analyze (typically by pattern matching) the result of recursive invocations of eval to check whether the result has the right type.
+This means that a new class of errors can occur: A subexpression evaluates to a number where a function is expected, or vice versa.
+Such errors are called _type errors_, and we will talk about them in much more detail later. For now, it means that we need to analyze
+(typically by pattern matching) the result of recursive invocations of eval to check whether the result has the right type.
 
 The remainder of the interpreter is unsurprising.
 */
@@ -178,14 +196,16 @@ val test = App( Fun('x,Add('x,5)), 7)
 assert( eval(test) == Num(12))
 
 /**
-FAE is a computationally (Turing)-complete language. For instance, we can define  a non-terminating program. This program is commonly called Omega */
+FAE is a computationally (Turing)-complete language. For instance, we can define  a non-terminating program. This program is commonly
+called Omega */
 
 val omega = App(Fun('x,App('x,'x)), Fun('x,App('x,'x)))
 
 // try eval(omega) to crash the interpreter ;-)
 
 /**
-Omega can be extended to yield a fixed point combinator, which can be used to encode arbitrary recursive functions. We come back to this topic later.
+Omega can be extended to yield a fixed point combinator, which can be used to encode arbitrary recursive functions. We come back to
+this topic later.
 
 Let's now discuss what an environment-based version of this interpreter looks like.
 
@@ -231,11 +251,14 @@ We could try to replace the second line in the "App" case by
      case Fun(x,body) => evalWithEnv0(body, env + (x -> evalWithEnv0(a,env)))
 but this would again introduce dynamic scoping.
 
-Hence, when we evaluate a function, we do not only have to store the function, but also the environment active when the function was created. This pair of function and environment is called a _closure_. The environment stored in the closure is used when the function is eventually applied.
+Hence, when we evaluate a function, we do not only have to store the function, but also the environment active when the function was
+created. This pair of function and environment is called a _closure_. The environment stored in the closure is used when the function
+is eventually applied.
 
 Hint: If you cannot answer what a closure is and how it is used in the  interpreter, you will be toast in the exam!
 
-Since closures are not expressible in the language syntax, we now come to the point where we need a separate category of _values_. The values in FAE can be either numbers or closures.
+Since closures are not expressible in the language syntax, we now come to the point where we need a separate category of _values_.
+The values in FAE can be either numbers or closures.
 */
 
 sealed abstract class Value
